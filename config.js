411dd8a15146080c1e77aeebb5e7a6ab51617d25
Ujjawal Garg <ujjawal.garg@sjsu.edu>
2018-06-15 05:21:46 -0700

    fix

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export const nodeEnv = env.NODE_ENV || 'development';
 
 
 export default {
-  pyEnvPath: nodeEnv == 'development' ? '/Users/gobletsky/.pyenv/versions/facemorph/bin/python' : '/root/.virtualenvs/facemorph/bin/python' ,
+  pyEnvPath: nodeEnv == 'development' ? '/Users/gobletsky/.pyenv/versions/facemorph/bin/python' : '/root/.virtualenvs/cv/bin/python' ,
   pyScriptPath: 'py_face_morph/',
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
